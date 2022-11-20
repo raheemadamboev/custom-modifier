de1b74c0c7ac8b3851328fdf6b69c33ca20be2b5
raheemadamboev <vividricemaryandmax@gmail.com>
2022-11-20 15:27:10 +0500

    install compose dependencies

--- app/src/main/java/xyz/teamgravity/custommodifier/Main.kt
+++ app/src/main/java/xyz/teamgravity/custommodifier/Main.kt
@@ -9,7 +9,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import xyz.teamgravity.custommodifier.ui.theme.CustomModifierTheme
 
-class MainActivity : ComponentActivity() {
+class Main : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
